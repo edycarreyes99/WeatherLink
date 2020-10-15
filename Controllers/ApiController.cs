fcb89bbe88f92c0266d22740312155e3da19719a
Edycar Reyes <edycarreyes@gmail.com>
2020-10-15 17:49:35 -0600

    Se configura correctamente el metodo para buscar entre todas las estaciones

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Globalization;
+using System.Linq;
 using System.Net.Mime;
 using System.Threading.Tasks;
 using Google.Apis.Util;
@@ -19,17 +20,21 @@ namespace WeatherLink.Controllers
             _apiDbContext = apiDbContext;
         }
 
-        // GET
+        // Ruta principal de la aplicacion
         [Route("")]
         [Route("Home")]
         [Route("Home/Index")]
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
         // Metodo que se ejecuta cuando se quiere extraer todas las estaciones
-        [Route("Estaciones/{id:int}")]
+        [Route("Estaciones")]
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> Estaciones()
         {
             return Ok(new
@@ -39,72 +44,7 @@ namespace WeatherLink.Controllers
             });
         }
 
-        // Metodo que se ejecuta cuando se quiere extraer todas las estaciones
-        [HttpGet]
-        [Route("Estacion")]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> Estacion(int id)
-        {
-            if (double.IsNaN(id))
-            {
-                return Json(new
-                {
-                    status = StatusCode(StatusCodes.Status400BadRequest).StatusCode,
-                    message = "El Id tiene que ser valido y es un parametro requerido."
-                });
-            }
 
-            return Ok(new
-            {
-                status = StatusCode(StatusCodes.Status200OK).StatusCode,
-                data = await _apiDbContext.Estaciones.FindAsync(id)
-            });
-        }
-
-        // Metodo que se ejecuta cuando se quiere agregar una nueva estacion
-        [HttpPost]
-        [Route("AgregarEstacion")]
-        [ProducesResponseType(StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> AgregarEstacion(string nombre, double latitud, double longitud)
-        {
-            ViewBag.nombre = nombre;
-            ViewBag.latitud = latitud;
-            ViewBag.longitud = longitud;
-
-            if (string.IsNullOrEmpty(nombre))
-            {
-                return Json(new
-                {
-                    status = StatusCode(StatusCodes.Status400BadRequest).StatusCode,
-                    message = "El nombre tiene que ser valido y es un parametro requerido."
-                });
-            }
-
-            if (double.IsNaN(latitud) || latitud == 0)
-            {
-                return BadRequest(Json(new
-                {
-                    status = StatusCode(StatusCodes.Status400BadRequest).StatusCode,
-                    message = "La latitud es un parametro requerido y tiene que ser un formato válido."
-                }));
-            }
-
-            if (double.IsNaN(longitud) || longitud == 0)
-            {
-                return BadRequest(Json(new
-                {
-                    status = StatusCode(StatusCodes.Status400BadRequest).StatusCode,
-                    message = "La longitud es un parametro requerido y tiene que ser un formato válido."
-                }));
-            }
-
-            return Ok(new
-            {
-                status = StatusCode(StatusCodes.Status201Created).StatusCode,
-                data = await _apiDbContext.Estaciones.ToListAsync()
-            });
-        }
+        
     }
 }
